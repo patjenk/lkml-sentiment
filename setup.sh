25377b133328c67144d9575deb441a658b532fd2
Cody Myers <codymyers63@gmail.com>
2018-01-29 19:57:01 +0000

    Added more libraries that are required for parse_email_quotes.py

--- setup.sh
+++ setup.sh
@@ -17,7 +17,10 @@ fi
 . venv/bin/activate
 
 # Python2
+python -m pip install --upgrade pip setuptools wheel
 sudo pip install beautifulsoup4==4.4.1
 sudo pip install requests==2.8.1
 sudo pip install wheel==0.24.0
 sudo pip install tqdm==4.19.5
+sudo pip install quotequail==0.2.3
+sudo pip install talon==1.4.4
